0bf32c5c13032aeea1695813a9520ab9f2dffcd8
brandcrz88 <brandonsamuel.cs@gmail.com>
2023-10-28 20:40:37 +0800

    24th commit for sap_1

--- src/config.tcl
+++ src/config.tcl
@@ -19,8 +19,8 @@ set ::env(PL_TARGET_DENSITY) 0.8
 set ::env(CLOCK_PERIOD) "100"
 
 # Hold slack margin - Increase them in case you are getting hold violations.
-set ::env(PL_RESIZER_HOLD_SLACK_MARGIN) 3
-set ::env(GLB_RESIZER_HOLD_SLACK_MARGIN) 0.1
+set ::env(PL_RESIZER_HOLD_SLACK_MARGIN) 100
+set ::env(GLB_RESIZER_HOLD_SLACK_MARGIN) 100
 
 # RUN_LINTER, LINTER_INCLUDE_PDK_MODELS - Disabling the linter is not recommended!
 set ::env(RUN_LINTER) 1
