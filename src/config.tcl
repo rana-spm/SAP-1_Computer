dc700714641c5b7fb268a3b1db56d03cccb8f02e
brandcrz88 <brandonsamuel.cs@gmail.com>
2023-10-28 19:08:59 +0800

    20th commit for sap_1

--- src/config.tcl
+++ src/config.tcl
@@ -12,15 +12,15 @@
 
 # PL_TARGET_DENSITY - You can increase this if Global Placement fails with error GPL-0302.
 # Users have reported that values up to 0.8 worked well for them.
-set ::env(PL_TARGET_DENSITY) 0.5
+set ::env(PL_TARGET_DENSITY) 0.6
 
 # CLOCK_PERIOD - Increase this in case you are getting setup time violations.
 # The value is in nanoseconds, so 20ns == 50MHz.
 set ::env(CLOCK_PERIOD) "100"
 
 # Hold slack margin - Increase them in case you are getting hold violations.
-set ::env(PL_RESIZER_HOLD_SLACK_MARGIN) 5
-set ::env(GLB_RESIZER_HOLD_SLACK_MARGIN) 4
+set ::env(PL_RESIZER_HOLD_SLACK_MARGIN) 0.3
+set ::env(GLB_RESIZER_HOLD_SLACK_MARGIN) 0.1
 
 # RUN_LINTER, LINTER_INCLUDE_PDK_MODELS - Disabling the linter is not recommended!
 set ::env(RUN_LINTER) 1
