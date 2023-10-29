f1f6755f0170cedb4a96b027c7eeaf09fa0a608f
brandcrz88 <brandonsamuel.cs@gmail.com>
2023-10-29 17:48:32 +0800

    Opcode timing fix.2

--- src/controller.v
+++ src/controller.v
@@ -25,10 +25,10 @@ localparam OP_ADD = 4'b0001;
 localparam OP_SUB = 4'b0010;
 localparam OP_HLT = 4'b1111;
 
-reg[2:0]  stage;
-reg[11:0] ctrl_word;
+reg [2:0]  stage;
+reg [11:0] ctrl_word;
 
-always @(negedge clk) begin
+always @(posedge clk) begin
 	if (rst) begin
 		stage <= 0;
 	end else begin
@@ -40,71 +40,81 @@ always @(negedge clk) begin
 	end
 end
 
+reg [11:0] ctrl_word_next; // New register for next stage values
+
 always @(posedge clk) begin
-	ctrl_word = 12'b0;
+	ctrl_word_next = 12'b0;
 
 	case (stage)
 		0: begin
-			ctrl_word[SIG_PC_EN] = 1;
-			ctrl_word[SIG_MEM_LOAD] = 1;
+			ctrl_word_next[SIG_PC_EN] = 1;
+			ctrl_word_next[SIG_MEM_LOAD] = 1;
 		end
 		1: begin
-			ctrl_word[SIG_PC_INC] = 1;
+			ctrl_word_next[SIG_PC_INC] = 1;
 		end
 		2: begin
-			ctrl_word[SIG_MEM_EN] = 1;
-			ctrl_word[SIG_IR_LOAD] = 1;
+			ctrl_word_next[SIG_MEM_EN] = 1;
+			ctrl_word_next[SIG_IR_LOAD] = 1;
 		end
 		3: begin
 			case (opcode)
 				OP_LDA: begin
-					ctrl_word[SIG_IR_EN] = 1;
-					ctrl_word[SIG_MEM_LOAD] = 1;
+					ctrl_word_next[SIG_IR_EN] = 1;
+					ctrl_word_next[SIG_MEM_LOAD] = 1;
 				end
 				OP_ADD: begin
-					ctrl_word[SIG_IR_EN] = 1;
-					ctrl_word[SIG_MEM_LOAD] = 1;
+					ctrl_word_next[SIG_IR_EN] = 1;
+					ctrl_word_next[SIG_MEM_LOAD] = 1;
 				end
 				OP_SUB: begin
-					ctrl_word[SIG_IR_EN] = 1;
-					ctrl_word[SIG_MEM_LOAD] = 1;
+					ctrl_word_next[SIG_IR_EN] = 1;
+					ctrl_word_next[SIG_MEM_LOAD] = 1;
 				end
 				OP_HLT: begin
-					ctrl_word[SIG_HLT] = 1;
+					ctrl_word_next[SIG_HLT] = 1;
 				end
 			endcase
 		end
 		4: begin
 			case (opcode)
 				OP_LDA: begin
-					ctrl_word[SIG_MEM_EN] = 1;
-					ctrl_word[SIG_A_LOAD] = 1;
+					ctrl_word_next[SIG_MEM_EN] = 1;
+					ctrl_word_next[SIG_A_LOAD] = 1;
 				end
 				OP_ADD: begin
-					ctrl_word[SIG_MEM_EN] = 1;
-					ctrl_word[SIG_B_LOAD] = 1;
+					ctrl_word_next[SIG_MEM_EN] = 1;
+					ctrl_word_next[SIG_B_LOAD] = 1;
 				end
 				OP_SUB: begin
-					ctrl_word[SIG_MEM_EN] = 1;
-					ctrl_word[SIG_B_LOAD] = 1;
+					ctrl_word_next[SIG_MEM_EN] = 1;
+					ctrl_word_next[SIG_B_LOAD] = 1;
 				end
 			endcase
 		end
 		5: begin
 			case (opcode)
 				OP_ADD: begin
-					ctrl_word[SIG_ADDER_EN] = 1;
-					ctrl_word[SIG_A_LOAD] = 1;
+					ctrl_word_next[SIG_ADDER_EN] = 1;
+					ctrl_word_next[SIG_A_LOAD] = 1;
 				end
 				OP_SUB: begin
-					ctrl_word[SIG_ADDER_SUB] = 1;
-					ctrl_word[SIG_ADDER_EN] = 1;
-					ctrl_word[SIG_A_LOAD] = 1;
+					ctrl_word_next[SIG_ADDER_SUB] = 1;
+					ctrl_word_next[SIG_ADDER_EN] = 1;
+					ctrl_word_next[SIG_A_LOAD] = 1;
 				end
 			endcase
 		end
 	endcase
 end
+
+always @(posedge clk) begin
+	if(rst)
+		ctrl_word <= 0;
+	else 
+		ctrl_word <= ctrl_word_next; // Transfer next stage values
+end
+
 
 assign out = ctrl_word;
 
